ff9413fc2e36b2a26318ca7daee8bfb7473b6248
SarraRocio <saratorralvocortes@gmail.com>
2026-05-07 23:55:08 -0500

    Upload Questions and Change app Name

--- preguntas.js
+++ preguntas.js
@@ -1,38 +1,38 @@
 // preguntas.js
 const preguntasDemo = [
     { 
-        text: "¿Qué lenguaje define la estructura de una web?", 
+        text: "¿Qué herramienta es ésta?", 
         type: "image", 
-        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/120px-HTML5_logo_and_wordmark.svg.png", 
-        options: ["HTML", "CSS", "JavaScript"], 
-        correct: "HTML" 
+        url: "https://bellota.b-cdn.net/CMP4506/PR4438BI12484_SA_127HPBM_MACHETE_127H_PULIDO_CACHA_BIMATERIAL_SZ6.png", 
+        options: ["Machete", "Pala", "Azadón"], 
+        correct: "Machete" 
     },
     { 
-        text: "¿Cuál es la capital de Italia?", 
+        text: "¿El proceso para sembrar una plantica, inicia con una cama de tierra, luego abro un huequito en el suelo, deposito la semilla y con el sol y agua crecera muy fuerte?", 
         type: "text", 
         url: "", 
-        options: ["Roma", "Milán", "Venecia"], 
-        correct: "Roma" 
+        options: ["✅Verdadero", "❌Falso"], 
+        correct: "✅Verdadero" 
     },
     { 
-        text: "Identifica este sonido instrumental", 
+        text: "Identifica este sonido", 
         type: "audio", 
-        url: "https://www.w3schools.com/html/horse.mp3", 
-        options: ["Caballo", "Vaca", "Oveja"], 
-        correct: "Caballo" 
+        url: "http://nature-downloads.naturesounds.ca/player/river_sounds_prev_naturesounds-ca.mp3", 
+        options: ["Agua fluyendo", "Vaca Mugiendo", "Perro Ladrando"], 
+        correct: "Agua fluyendo" 
     },
     { 
-        text: "¿Qué propiedad de CSS cambia el color de fondo?", 
-        type: "text", 
-        url: "", 
-        options: ["background-color", "color", "border-style"], 
-        correct: "background-color" 
+        text: "¿A dónde irías para jugar y descansar?", 
+        type: "image", 
+        url: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjBGPhg2uj-rLN5HaswQkNfXmXzRMtUV3gWjaZ_04JM7EdpvZYKNQL6BhHWR0s1omoV8iRMi_kQ-S53ekmhN6zPWQ4uCkGzNk27h85WunILxhuJMlWqiXyaGBHNAhm0L0zah_3gqPNtrvjrPS5dps4deaEPt4NbYrUHDlYXlW4t76H2Ta9QxvOW-xrXF_eu/s16000/children-playground-with-swings-slide-climbing-ladders-and-more-in-the-amusement-park-for-little-ones-to-play-in-flat-cartoon-illustration-vector.jpg", 
+        options: ["Parque", "Puesto de Salud", "Escuela"], 
+        correct: "Parque" 
     },
     { 
-        text: "¿Cuál es el resultado de 10 / 2?", 
-        type: "text", 
-        url: "", 
-        options: ["5", "2", "10"], 
-        correct: "5" 
+        text: "¿Qué escena es ésta?", 
+        type: "image", 
+        url: "https://media.istockphoto.com/id/1344645252/es/foto/caficultor-caminando-por-la-plantaci%C3%B3n-de-caf%C3%A9-plantas-de-caf%C3%A9-hombre-con-sombrero-de-campesino.jpg?s=1024x1024&w=is&k=20&c=Q2tqdc4PlmAVC-nuyAIlk91BHC79ig01dzL2Q3SYzjE=", 
+        options: ["Cafetal", "Parque", "Siembra"], 
+        correct: "Cafetal" 
     }
 ];

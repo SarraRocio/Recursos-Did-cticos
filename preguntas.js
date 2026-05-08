8a1e7a2d3d465ffeace2db0902b3de42c3abc076
SarraRocio <saratorralvocortes@gmail.com>
2026-05-07 21:03:39 -0500

    refactor Logic, import questions from file

---
+++ preguntas.js
@@ -0,0 +1,38 @@
+// preguntas.js
+const preguntasDemo = [
+    { 
+        text: "¿Qué lenguaje define la estructura de una web?", 
+        type: "image", 
+        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/120px-HTML5_logo_and_wordmark.svg.png", 
+        options: ["HTML", "CSS", "JavaScript"], 
+        correct: "HTML" 
+    },
+    { 
+        text: "¿Cuál es la capital de Italia?", 
+        type: "text", 
+        url: "", 
+        options: ["Roma", "Milán", "Venecia"], 
+        correct: "Roma" 
+    },
+    { 
+        text: "Identifica este sonido instrumental", 
+        type: "audio", 
+        url: "https://www.w3schools.com/html/horse.mp3", 
+        options: ["Caballo", "Vaca", "Oveja"], 
+        correct: "Caballo" 
+    },
+    { 
+        text: "¿Qué propiedad de CSS cambia el color de fondo?", 
+        type: "text", 
+        url: "", 
+        options: ["background-color", "color", "border-style"], 
+        correct: "background-color" 
+    },
+    { 
+        text: "¿Cuál es el resultado de 10 / 2?", 
+        type: "text", 
+        url: "", 
+        options: ["5", "2", "10"], 
+        correct: "5" 
+    }
+];
